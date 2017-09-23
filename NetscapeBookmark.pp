e6e1de459f0cd04b41cb2dc672d04586ee116262
VirtualTam <virtualtam@flibidi.net>
2017-09-23 22:49:24 +0200

    Grammar: string, title, subtitle
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- NetscapeBookmark.pp
+++ NetscapeBookmark.pp
@@ -1,15 +1,34 @@
 // Netscape Bookmark grammar
 %skip   space         \s
 
+// Generic tokens
+%token  string         \w+(\s\w+)*
+
 // Doctype
 %token  doctype_       <!(?i)doctype -> doctype
 %skip   doctype:space  \s
 %token  doctype:string \w+(-\w+)*
 %token  doctype:_t     >             -> default
 
+// Titles
+%token  title_        <(?i)title>
+%token _title         </(?i)title>
+
+%token  subtitle_     <(?i)h1>
+%token _subtitle      </(?i)h1>
+
+
 // Document structure
 #document:
     doctype()
+    title()
+    subtitle()
 
 #doctype:
     ::doctype_:: <string> ::_t::
+
+#title:
+    ::title_:: <string> ::_title::
+
+#subtitle:
+    ::subtitle_:: <string> ::_subtitle::
