9a6012f3983f34e15da623b6a8cf1af2bafd683b
VirtualTam <virtualtam@flibidi.net>
2017-09-23 22:41:00 +0200

    Grammar: document + doctype
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- NetscapeBookmark.pp
+++ NetscapeBookmark.pp
@@ -1,1 +1,15 @@
 // Netscape Bookmark grammar
+%skip   space         \s
+
+// Doctype
+%token  doctype_       <!(?i)doctype -> doctype
+%skip   doctype:space  \s
+%token  doctype:string \w+(-\w+)*
+%token  doctype:_t     >             -> default
+
+// Document structure
+#document:
+    doctype()
+
+#doctype:
+    ::doctype_:: <string> ::_t::
